cb0303d7733d7b87fc68716ee9b310f3d0e8bbd7
Graham Knop <haarg@haarg.org>
2014-06-02 16:35:04 -0400

    add pod basics

--- lib/MooX/Clone.pm
+++ lib/MooX/Clone.pm
@@ -21,3 +21,45 @@ sub import {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+MooX::Clone - Provide a method for cloning Moo objects
+
+=head1 SYNOPSIS
+
+    package MyClass;
+    use Moo;
+    use MooX::Clone;
+
+    has attr1 => (is => 'ro');
+    has attr2 => (is => 'ro');
+
+    my $o = MyClass->new(attr1 => 1, attr2 => 2);
+    my $o2 = $o->but(attr1 => 5);
+
+=head1 DESCRIPTION
+
+MooX::Clone provides a method that will clone an object, while setting new
+attribute values.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head2 CONTRIBUTORS
+
+None so far.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014 the MooX::Clone L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
+
+=cut
