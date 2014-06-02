dbcd317a877eec71519e9075fefce77dd230af62
Graham Knop <haarg@haarg.org>
2014-06-02 17:18:25 -0400

    more docs

--- lib/MooX/Clone.pm
+++ lib/MooX/Clone.pm
@@ -34,20 +34,44 @@ MooX::Clone - Provide a method for cloning Moo objects
 
 =head1 SYNOPSIS
 
-    package MyClass;
-    use Moo;
-    use MooX::Clone;
+  package ClonableClass;
+  use Moo;
+  use MooX::Clone;
 
-    has attr1 => (is => 'ro');
-    has attr2 => (is => 'ro');
+  has attr1 => (is => 'ro');
+  has attr2 => (is => 'ro', clone => 0);
+  has attr3 => (is => 'ro', clone => 'deep');
+  has attr4 => (is => 'ro', clone => 0, required => 1);
 
-    my $o = MyClass->new(attr1 => 1, attr2 => 2);
-    my $o2 = $o->but(attr1 => 5);
+  my $o = ClonableClass->new(attr1 => 1, attr2 => 2, attr3 => {}, attr4 => 4);
+  my $o2 = $o->but(attr4 => 5);
+  # attr1 will be 1
+  # attr2 will be unset
+  # attr3 will have a new hashref with the same content as $o->attr3
+  # attr4 will be 4.  if not provided to ->but, will trigger an error
+
+
+  # use alternate method name
+  package ClonableClassRenamed;
+  use Moo;
+  use MooX::Clone method => 'clone';
 
 =head1 DESCRIPTION
 
 MooX::Clone provides a method that will clone an object, while setting new
 attribute values.
+
+=head1 CAVEATS
+
+=over 4
+
+=item * New attributes are processed by BUILDARGS
+
+The arguments passed to C<< ->but >> are processed by C<BUILDARGS> just like
+arguments to C<new> are.  If the class has a C<BUILDARGS> method, it will have
+to be written to account for this.
+
+=back
 
 =head1 AUTHOR
 
