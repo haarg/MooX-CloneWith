d13cc4ecd9cdb57b5ecbf7eaf133261ffda45ca6
Graham Knop <haarg@haarg.org>
2016-10-15 16:31:29 -0400

    update docs

--- lib/MooX/CloneWith.pm
+++ lib/MooX/CloneWith.pm
@@ -48,8 +48,7 @@ MooX::CloneWith - Provide a method for cloning Moo objects
   # attr1 will be 1
   # attr2 will be unset
   # attr3 will have a new hashref with the same content as $o->attr3
-  # attr4 will be 4.  if not provided to ->but, will trigger an error
-
+  # attr4 will be 4.  if not provided to ->clone_with, will trigger an error
 
   # use alternate method name
   package ClonableClassRenamed;
@@ -61,19 +60,68 @@ MooX::CloneWith - Provide a method for cloning Moo objects
 C<MooX::CloneWith> provides a method that will clone an object, while setting new
 attribute values.
 
+The method generated accepts either hashref of attributes, or a list of key value
+pairs.  This can be customized by implementing a C<CLONEARGS> method, which
+will be given the list of paramters and is expected to return a hashref.
+
+=head1 IMPORT OPTIONS
+
+When importing B<MooX::CloneWith>, two options can be specified:
+
+=over 4
+
+=item method
+
+The method name to generate to clone the class.  If not specified, the method
+name will be C<clone_with>.
+
+=item type
+
+The type of cloning that will be used by default.  The default is C<copy>.
+
+=back
+
+=head1 ATTRIBUTE OPTIONS
+
+=head2 clone
+
+The clone option accepts several values for how to clone the attribute value.
+
+=over 4
+
+=item false
+
+A false value means that the attribute will not be cloned.
+
+=item copy
+
+The attribute will be copied directly, including references.  This means that
+the modifications to hashrefs or arrayrefs in the new object will also effect
+the old object.
+
+=item clone
+
+The attribute value will be cloned.  New array and hash referenced will be
+created, with copies of the old references values.
+
+Objects will attempt to be cloned using either method given at import,
+C<clone_with>, C<clone>, or using L<Storable> if hooks for it have been
+implemented.  If none of these are available, an error will be thrown.
+
+=item $coderef
+
+An arbitrary code reference can be given, and it will be called to clone the
+attribute.
+
+=back
+
 =head1 CAVEATS
 
 =over 4
 
-=item * New attributes are processed by BUILDARGS
-
-The arguments passed to C<< ->clone_with >> are processed by C<BUILDARGS> just like
-arguments to C<new> are.  If the class has a C<BUILDARGS> method, it will have
-to be written to account for this.
-
 =item * Only works with full Moo classes
 
-The but method provided will not work properly if the class inherits from a
+The clone method provided will not work properly if the class inherits from a
 non-Moo class, or for subclasses that use Moose.
 
 =back
