5f39b2b98a86e54417e37b9aa63e64c41736459c
Graham Knop <haarg@haarg.org>
2016-10-15 11:42:19 -0400

    significant refactoring

--- lib/MooX/CloneWith/Role/GenerateConstructor.pm
+++ lib/MooX/CloneWith/Role/GenerateConstructor.pm
@@ -1,9 +1,11 @@
 package MooX::CloneWith::Role::GenerateConstructor;
-use Sub::Quote qw(quote_sub unquote_sub);
+use Sub::Quote qw(quote_sub unquote_sub quotify sanitize_identifier);
 use Sub::Defer qw(defer_sub);
+use Carp;
 use Moo::Role;
 
-sub clone_method { 'but' }
+sub clone_method { 'clone_with' }
+sub default_clone_type { 'copy' }
 
 after install_delayed => sub {
   my ($self) = @_;
@@ -15,96 +17,144 @@ sub install_delayed_clone {
   my $package = $self->{package};
   my $clone_method = $self->clone_method;
   defer_sub "${package}::${clone_method}" => sub {
-    unquote_sub $self->generate_clone_method(
-      $package, $clone_method, $self->{attribute_specs}
+    $self->_generate_clone_method(
+      $package, $clone_method, $self->{attribute_specs}, { no_defer => 1 },
     );
   };
 };
-
-sub generate_clone_method {
-  my ($self, $into, $name, $spec, $quote_opts) = @_;
-  local $self->{captures} = {};
-  my $body = '    my $self = shift;'."\n"
-            .'    my $class = ref($self);'."\n";
-
-  my $into_buildargs = $into->can('BUILDARGS');
-  if ( $into_buildargs && $into_buildargs != \&Moo::Object::BUILDARGS ) {
-      $body .= $self->_generate_args_via_buildargs;
-  } else {
-      $body .= $self->_generate_args;
-  }
-  $body .= $self->_check_required($self->_required_clone_spec($spec));
-  $body .= '    my $new = '.$self->construction_string.";\n";
-  $body .= $self->_assign_clone($spec);
-  $body .= $self->_assign_new($spec);
-  if ($into->can('BUILD')) {
-    $body .= $self->buildall_generator->buildall_body_for(
-      $into, '$new', '$args'
-    );
-  }
-  $body .= '    return $new;'."\n";
-  quote_sub
-    "${into}::${name}" => $body,
-    $self->{captures}, $quote_opts||{}
-  ;
-}
-
-sub _assign_clone {
-  my ($self, $spec) = @_;
-  my $ag = $self->accessor_generator;
-  my %test;
-  foreach my $name (sort keys %$spec) {
-    my $attr_spec = $spec->{$name};
-    next
-      if !$self->_is_clonable($attr_spec);
-    $test{$name} = $name;
-  }
-  join '', map {
-    my $attr_spec = $spec->{$_};
-    local $ag->{captures} = {};
-    my $test = $ag->_generate_simple_has('$self', $_);
-    my $source = $ag->_generate_simple_get('$self', $_);
-    if ($attr_spec->{clone} && $attr_spec->{clone} eq 'deep') {
-      $source = $self->_generate_deep_clone($source);
-    }
-    my $set = $ag->_generate_simple_set('$new', $_, $attr_spec, $source);
-    $self->_cap_call("$set if $test;\n", delete $ag->{captures});
-  } sort keys %test;
-}
-
-sub _generate_deep_clone {
-  my ($self, $source) = @_;
-  my $clone_method = $self->clone_method;
-  'do {'.
-  '  my $v = '.$source.';'."\n".
-  '    !ref($v)           ? $v'."\n".
-  '  : Scalar::Util::blessed($v) ? ('."\n".
-  ( $clone_method eq 'but' ? '' :
-  '      $v->can("'.$clone_method.'")   ? $v->'.$clone_method."\n"
-  ).
-  '      $v->can("but")   ? $v->but'."\n".
-  '    : $v->can("clone") ? $v->clone'."\n".
-  '    : die "Can\'t clone $v"'."\n".
-  '  )'."\n".
-  '  : ref($v) eq "ARRAY" ? [%$v]'."\n".
-  '  : ref($v) eq "HASH"  ? {%$v}'."\n".
-  '  : $v;'."\n".
-  '}';
-}
-
-sub _is_clonable {
-  my ($self, $spec) = @_;
-  return 0 if exists $spec->{clone} && !$spec->{clone};
-  return 1;
-}
 
 sub _required_clone_spec {
   my ($self, $spec) = @_;
   +{
     map {; $_ => $spec->{$_} }
-    grep !$self->_is_clonable($spec->{$_}),
+    grep !$self->_clone_type($spec->{$_}),
     keys %$spec
   }
+}
+
+sub _generate_clone_method {
+  my ($self, $into, $name, $spec, $quote_opts) = @_;
+  local $self->{captures} = {};
+  my $body
+    = '    my $self = shift;'."\n"
+    . '    my $class = ref($self);'."\n"
+    . ( $into->can('CLONEARGS') ? (
+       q{    my $args = $class->CLONEARGS(@_);}."\n"
+      .q{    Carp::croak("CLONEARGS did not return a hashref") unless CORE::ref($args) eq 'HASH';}."\n"
+    ) : $self->_generate_args)
+    . $self->_check_required($self->_required_clone_spec($spec))
+    . '    my $new = '.$self->construction_string.";\n"
+    . $self->_assign_clone($spec, '$self', '$new', '$args')
+    . $self->_assign_new($spec)
+    . ( $into->can('BUILD')
+      ? $self->buildall_generator->buildall_body_for($into, '$new', '$args')
+      : ''
+    )
+    . '    (return $new);'."\n";
+  quote_sub
+    "${into}::${name}" => $body,
+    $self->{captures}, $quote_opts||{},
+  ;
+}
+
+sub _assign_clone {
+  my ($self, $spec, $from, $to, $source) = @_;
+  my %test;
+  NAME: foreach my $name (sort keys %$spec) {
+    my $attr_spec = $spec->{$name};
+    next
+      unless $self->_clone_type($attr_spec);
+    $test{$name} = $attr_spec;
+  }
+  join '', map {
+    my $attr_spec = $test{$_};
+    my $test_arg = exists $attr_spec->{init_arg} ? $attr_spec->{init_arg} : $_;
+    my $test = (defined $test_arg ? '!exists '.$source.'->{'.quotify($test_arg).'}' : undef);
+    $self->_generate_attr_clone($from, $to, $_, $attr_spec, $test, $test_arg);
+  } sort keys %test;
+}
+
+# the rest of this should probably be on an accessor generator role
+sub _generate_attr_clone {
+  my ($self, $from, $to, $attr, $spec, $test, $test_arg) = @_;
+  my $ag = $self->accessor_generator;
+  my $type = $self->_clone_type($spec);
+  $test = ($test ? "$test && " : '')
+    . $ag->_generate_simple_has($from, $_);
+  my $source = $self->_generate_clone_type($attr,
+    $ag->_generate_simple_get($from, $attr), $type);
+  my $set = $ag->_generate_simple_set($to, $attr, $spec, $source);
+  "($test and $set),\n";
+}
+
+sub _generate_clone_type {
+  my ($self, $attr, $source, $type) = @_;
+  if ($type eq 'copy') {
+    return $source;
+  }
+  elsif (ref $type && eval { \&$type }) {
+  }
+  elsif ($self->can(my $method = '_generate_clone_type_'.$type)) {
+    $type = $self->$method($attr);
+  }
+  else {
+    croak "Unknown clone type $type for $attr";
+  }
+  my $var = '$_clone_captures_for_'.sanitize_identifier($attr);
+  $self->{captures}{$var} = \$type;
+  return $var.'->('.$source.')';
+}
+
+sub _generate_clone_type_clone {
+  my ($self, $attr) = @_;
+  my $clone_method = $self->clone_method;
+  sub {
+    my $v = shift;
+    my $vtype = ref $v;
+    return
+        !$vtype ? $v
+      : $vtype eq "Regexp" ? $v
+      : Scalar::Util::blessed($v) ? (
+          ( $clone_method ne 'clone_with' && $v->can($clone_method) ) ? $v->$clone_method
+        : $v->can("clone_with")  ? $v->clone_with
+        : $v->can("clone")       ? $v->clone
+        : ($v->can("STORABLE_freeze") && $v->can("STORABLE_thaw"))
+          ? ((require Storable), Storable::dclone($v))
+        : Carp::croak("Can't clone attribute $attr: $v")
+      )
+      : $vtype eq "ARRAY" ? [@$v]
+      : $vtype eq "HASH"  ? {%$v}
+      : ($vtype eq "SCALAR" || $vtype eq "REF" || $vtype eq "VSTRING" || $vtype eq "LVALUE")
+        ? \(my ($c) = $$v)
+      : $v;
+  };
+}
+
+sub _generate_clone_type_deep {
+  my ($self, $attr) = @_;
+  my $clone = $self->_generate_clone_type_clone($attr);
+  sub {
+    my $vo = shift;
+    my @clone = (\$vo);
+    while (my $vref = pop @clone) {
+      my $v = $$vref = $clone->($$vref);
+      my $vtype = ref $v;
+      push @clone,
+        Scalar::Util::blessed($v) ? ()
+        : $vtype eq "ARRAY" ? \(@$v)
+        : $vtype eq "HASH"  ? \(values %$v)
+        : $vtype eq "REF"   ? $v
+        : ();
+    }
+    $vo;
+  };
+}
+
+sub _clone_type {
+  my ($self, $spec) = @_;
+  exists $spec->{clone} ? (
+    (($spec->{clone}||0) eq 1) ? 'copy' : $spec->{clone}
+  ) : $self->default_clone_type;
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -13,7 +13,7 @@ use Test::Fatal;
 
 {
   my $o = ClonableBasic->new(foo => 1, bar => 2);
-  my $o2 = $o->but(foo => 5);
+  my $o2 = $o->clone_with(foo => 5);
   is $o->foo, 1, 'initial attribute unmodified';
   is $o->bar, 2, 'initial attribute unmodified';
   is $o2->foo, 5, 'cloned attribute overwritten';
@@ -25,14 +25,28 @@ use Test::Fatal;
   use Moo;
   use MooX::CloneWith;
   has foo => (is => 'ro');
-  has bar => (is => 'ro', clone => 'deep');
+  has bar => (is => 'ro', clone => 'clone');
+  has baz => (is => 'ro', clone => 'deep');
 }
 
 {
-  my $o = ClonableDeep->new(foo => {}, bar => {});
-  my $o2 = $o->but;
+  my $o = ClonableDeep->new(
+    foo => {},
+    bar => {
+      buzz => {},
+    },
+    baz => {
+      buzz => {},
+    },
+  );
+  my $o2 = $o->clone_with;
   is $o->foo, $o2->foo, 'normal cloned attribute uses same reference';
-  isnt $o->bar, $o2->bar, 'deep cloned attribute gets new reference';
+  isnt $o->bar, $o2->bar, 'shallow cloned attribute gets new reference';
+  is $o->bar->{buzz}, $o2->bar->{buzz},
+    'shallow cloned attribute inner refs maintained';
+  isnt $o->baz, $o2->baz, 'deep cloned attribute gets new reference';
+  isnt $o->baz->{buzz}, $o2->baz->{buzz},
+    'deep cloned attribute inner refs get new reference';
 }
 
 {
@@ -45,7 +59,7 @@ use Test::Fatal;
 
 {
   my $o = ClonableNoClone->new(foo => 1, bar => 2);
-  my $o2 = $o->but(foo => 5);
+  my $o2 = $o->clone_with(foo => 5);
   is $o->foo, 1, 'initial attribute unmodified';
   is $o->bar, 2, 'initial unclonable attribute unmodified';
   is $o2->foo, 5, 'cloned attribute overwritten';
@@ -62,7 +76,7 @@ use Test::Fatal;
 
 {
   my $o = ClonableRequired->new(foo => 1, bar => 2);
-  like exception { $o->but }, qr/Missing required arguments: bar at/,
+  like exception { $o->clone_with }, qr/Missing required arguments: bar at/,
     'unclonable required attribute must be provided on clone';
 }
 
